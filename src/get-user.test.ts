339a46db8f7fb46bcfb83f3a4c4246727092f799
Mesgan Dargie <mesgan.dargie@gmail.com>
2021-12-08 17:04:44 -0600

    Added .vscode to .gitignore

--- src/get-user.test.ts
+++ src/get-user.test.ts
@@ -1,10 +1,9 @@
 import { getUser } from "./get-user";
 
-describe('when everthing is ok', () => {
-    test('should return a response', async () => {
-        //In a real project, you would use axios and mock the get method
-        const result = await getUser();
-        expect(result).toEqual({id:'1',name:'David'});
-    })
-    
-})
+describe("when everthing is ok", () => {
+  test("should return a response", async () => {
+    //In a real project, you would use axios and mock the get method
+    const result = await getUser();
+    expect(result).toEqual({ id: "1", name: "David" });
+  });
+});
